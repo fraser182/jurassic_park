f09d6206dd25a49c11a6d0888769f545b0f9e044
Fraser Ross <fraser182@icloud.com>
2018-12-18 17:08:21 +0000

    first commit

---
+++ models/park.js
@@ -0,0 +1,8 @@
+const Park = function (name, ticketPrice, collectionOfDinoaurs) {
+  this.name = name;
+  this.ticketPrice = ticketPrice;
+  this.collectionOfDinosaurs = collectionOfDinosaurs;
+
+}
+
+module.exports = Park;
